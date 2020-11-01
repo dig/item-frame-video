25a6687056ab0a693ca9b87f3f5c3016ce3e27e5
Joseph <joseph42@live.co.uk>
2020-11-01 01:38:26 +0000

    Initial repo

---
+++ src/main/java/com/github/dig/video/ItemFrameVideoPlugin.java
@@ -0,0 +1,11 @@
+package com.github.dig.video;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ItemFrameVideoPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
